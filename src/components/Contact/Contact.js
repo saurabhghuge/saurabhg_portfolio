33aee936912fdad35ca186e799be756e7992de20
SaurabhGhuge98 <saurabhghuge1998@gmail.com>
2022-08-04 12:22:47 +0530

    code formatting

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,6 +7,7 @@ import { useForm } from 'react-hook-form';
 import emailjs from 'emailjs-com';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
+import { Button } from "react-bootstrap";
 
 
 
@@ -65,184 +66,187 @@ const Contact = () => {
   return (
     <div className='frontpage bg-dark contact'  id="Contact">
 
-<div className='ContactForm'>
-      <div className='container'>
-      <div className='col-12 text-center copyright mt-5'>
-           {/* <h1 className="main-color">
-           Connect with me 
-           </h1> */}
-           <h1 className='section-title'>
-              <span className='main-color'>Connect with me </span>
-            </h1>
-           
-               <span className='icons'>
-                   <a href="https://www.linkedin.com/in/saurabhghuge/">
-                       <LinkedIn className='icon' />
-                   </a>
-               </span>
-               <span className='icons'>
-                   <a href="https://github.com/saurabhghuge">
-                       <Github className='icon' />
-                   </a>
-               </span>
-               <span className='icons'>
-                   <a href="https://medium.com/@saurabhghuge/">
-                       <Medium className='icon' />
-                   </a>
-               </span>
-               <span className='icons'>
-                   <a href="https://twitter.com/SaurabhGhuge16">
-                       <Twitter className='icon' />
-                   </a>
-               </span>
-
-           </div>
-        {/* Row 1 */}
-        <div className='row'>
-          <div className='col-12'>
+  <div className='ContactForm'>
+        <div className='container'>
+        <div className='col-12 text-center copyright mt-5'>
+            {/* <h1 className="main-color">
+            Connect with me 
+            </h1> */}
             <h1 className='section-title'>
-              <span className='main-color'>Contact  Me</span>
-            </h1>
-          </div>
-        </div>
-        {/* Row 2 */}
-        <div className='row'>
-          <div className='col-lg-4'>
-            <div className='contact-info'>
-              <div className='phone-info'>
-                {/* <FontAwesomeIcon
-                  icon={faPhone}
-                  className='contact-icon'
-                  fixedWidth
-                ></FontAwesomeIcon> */}
-                <span>Give me a call:</span>
-                <p>+91 8108381738</p>
-              </div>
-            </div>
-          </div>
-          <div className='col-lg-4'>
-            <div className='contact-info'>
-              <div className='email-info'>
-                {/* <FontAwesomeIcon
-                  icon={faEnvelope}
-                  className='contact-icon'
-                  fixedWidth
-                ></FontAwesomeIcon> */}
-                <span>Send me an email:</span>
-                <p>itsgsaurabh@gmail.com</p>
-              </div>
-            </div>
-          </div>
-          <div className='col-lg-4'>
-            <div className='contact-info'>
-              <div className='location-info'>
-                {/* <FontAwesomeIcon
-                  icon={faMapMarkerAlt}
-                  className='contact-icon'
-                  fixedWidth
-                ></FontAwesomeIcon> */}
-                <span>Currently located:</span>
-                <p>Mumbai,India</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                <span className='main-color'>Connect with me </span>
+              </h1>
+            
+                <span className='icons'>
+                    <a href="https://www.linkedin.com/in/saurabhghuge/">
+                        <LinkedIn className='icon' />
+                    </a>
+                </span>
+                <span className='icons'>
+                    <a href="https://github.com/saurabhghuge">
+                        <Github className='icon' />
+                    </a>
+                </span>
+                <span className='icons'>
+                    <a href="https://medium.com/@saurabhghuge/">
+                        <Medium className='icon' />
+                    </a>
+                </span>
+                <span className='icons'>
+                    <a href="https://twitter.com/SaurabhGhuge16">
+                        <Twitter className='icon' />
+                    </a>
+                </span>
 
-      <div className='Contact container'>
-        <div className='row'>
-          <div className='col-12 text-center'>
-            <div className='contactForm'>
-              <form id='contact-form' className='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
-                {/* Row 1 of form */}
-                <div className='row formRow'>
-                  <div className='col-6'>
-                    <input
-                      type='text'
-                      name='name'
-                      {...register('name', {
-                        required: { value: true, message: 'Please enter your name' },
-                        maxLength: {
-                          value: 30,
-                          message: 'Please use 30 characters or less'
-                        }
-                      })}
-                      className='form-control formInput'
-                      placeholder='Name'
-                    ></input>
-                    {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
-                  </div>
-                  <div className='col-6'>
-                    <input
-                      type='email'
-                      name='email'
-                      {...register('email', {
-                        required: true,
-                        pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-                      })}
-                      className='form-control formInput'
-                      placeholder='Email address'
-                    ></input>
-                    {errors.email && (
-                      <span className='errorMessage'>Please enter a valid email address</span>
-                    )}
-                  </div>
+            </div>
+          {/* Row 1 */}
+          <div className='row'>
+            <div className='col-12'>
+              <h1 className='section-title'>
+                <span className='main-color'>Contact  Me</span>
+              </h1>
+            </div>
+          </div>
+          {/* Row 2 */}
+          <div className='row'>
+            <div className='col-lg-4'>
+              <div className='contact-info'>
+                <div className='phone-info'>
+                  {/* <FontAwesomeIcon
+                    icon={faPhone}
+                    className='contact-icon'
+                    fixedWidth
+                  ></FontAwesomeIcon> */}
+                  <span>Give me a call:</span>
+                  <p>+91 8108381738</p>
                 </div>
-                {/* Row 2 of form */}
-                {/* <div className='row formRow'>
-                  <div className='col'>
-                    <input
-                      type='text'
-                      name='subject'
-                      {...register('subject', {
-                        required: { value: true, message: 'Please enter a subject' },
-                        maxLength: {
-                          value: 75,
-                          message: 'Subject cannot exceed 75 characters'
-                        }
-                      })}
-                      className='form-control formInput'
-                      placeholder='Subject'
-                    ></input>
-                    {errors.subject && (
-                      <span className='errorMessage'>{errors.subject.message}</span>
-                    )}
-                  </div>
-                </div> */}
-                {/* Row 3 of form */}
-                <div className='row formRow'>
-                  <div className='col'>
-                    <textarea
-                      rows={3}
-                      name='message'
-                      {...register('message', {
-                        required: true
-                      })}
-                      className='form-control formInput'
-                      placeholder='Message'
-                    ></textarea>
-                    {errors.message && <span className='errorMessage'>Please enter a message</span>}
-                  </div>
-                </div>
-                <button className='submit-btn' type='submit'>
-                  Submit
-                </button>
-              </form>
+              </div>
             </div>
-            <ToastContainer />
+            <div className='col-lg-4'>
+              <div className='contact-info'>
+                <div className='email-info'>
+                  {/* <FontAwesomeIcon
+                    icon={faEnvelope}
+                    className='contact-icon'
+                    fixedWidth
+                  ></FontAwesomeIcon> */}
+                  <span>Send me an email:</span>
+                  <p>itsgsaurabh@gmail.com</p>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4'>
+              <div className='contact-info'>
+                <div className='location-info'>
+                  {/* <FontAwesomeIcon
+                    icon={faMapMarkerAlt}
+                    className='contact-icon'
+                    fixedWidth
+                  ></FontAwesomeIcon> */}
+                  <span>Currently located:</span>
+                  <p>Mumbai,India</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className='Footer'>
-      <div className='col-12 text-center purple copyright mt-5'>
-        <p>Copyright &copy; {new Date().getFullYear()} Saurabh Ghuge</p>
+        <div className='Contact container'>
+          <div className='row'>
+            <div className='col-12 text-center'>
+              <div className='contactForm'>
+                <form id='contact-form' className='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
+                  {/* Row 1 of form */}
+                  <div className='row formRow'>
+                    <div className='col-6'>
+                      <input
+                        type='text'
+                        name='name'
+                        {...register('name', {
+                          required: { value: true, message: 'Please enter your name' },
+                          maxLength: {
+                            value: 30,
+                            message: 'Please use 30 characters or less'
+                          }
+                        })}
+                        className='form-control formInput'
+                        placeholder='Name'
+                      ></input>
+                      {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
+                    </div>
+                    <div className='col-6'>
+                      <input
+                        type='email'
+                        name='email'
+                        {...register('email', {
+                          required: true,
+                          pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                        })}
+                        className='form-control formInput'
+                        placeholder='Email address'
+                      ></input>
+                      {errors.email && (
+                        <span className='errorMessage'>Please enter a valid email address</span>
+                      )}
+                    </div>
+                  </div>
+                  {/* Row 2 of form */}
+                  {/* <div className='row formRow'>
+                    <div className='col'>
+                      <input
+                        type='text'
+                        name='subject'
+                        {...register('subject', {
+                          required: { value: true, message: 'Please enter a subject' },
+                          maxLength: {
+                            value: 75,
+                            message: 'Subject cannot exceed 75 characters'
+                          }
+                        })}
+                        className='form-control formInput'
+                        placeholder='Subject'
+                      ></input>
+                      {errors.subject && (
+                        <span className='errorMessage'>{errors.subject.message}</span>
+                      )}
+                    </div>
+                  </div> */}
+                  {/* Row 3 of form */}
+                  <div className='row formRow'>
+                    <div className='col'>
+                      <textarea
+                        rows={3}
+                        name='message'
+                        {...register('message', {
+                          required: true
+                        })}
+                        className='form-control formInput'
+                        placeholder='Message'
+                      ></textarea>
+                      {errors.message && <span className='errorMessage'>Please enter a message</span>}
+                    </div>
+                  </div>
+                  <Button variant="primary" size="md" type='submit'>
+                        Submit
+                  </Button>
+                  {/* <button className='submit-btn' type='submit'>
+                    Submit
+                  </button> */}
+                </form>
+              </div>
+              <ToastContainer />
+            </div>
+          </div>
+        </div>
+
+        <div className='Footer'>
+        <div className='col-12 text-center purple copyright mt-5'>
+          <p>Copyright &copy; {new Date().getFullYear()} Saurabh Ghuge</p>
+        </div>
+
+
+            
       </div>
-
-
-           
-    </div>
     </div>
   );
 };
--- src/components/FrontPage/FrontPage.js
+++ src/components/FrontPage/FrontPage.js
@@ -1,14 +1,9 @@
 import React from "react";
 import './FrontPage.css';
 import CoderImg  from './images/Saurabh.jpeg';
-import Projects  from "../Projects/Projects";
-import Experience from "../Experience/Experience";
-import Blogs from "../Blogs/Blogs";
 import Typewriter from 'typewriter-effect';
-// import Fade from 'react-reveal';
-
 import { Button } from "react-bootstrap";
-
+import {AiOutlineArrowDown} from "react-icons/ai";
 
 const styles = {
     nameStyle: {
@@ -17,6 +12,8 @@ const styles = {
     },
     inlineChild: {
       display: 'inline-block',
+    //   fontSize: '0.5em',
+
     },
     mainContainer: {
     //   margin: '50px,0px,0px,0px',
@@ -30,66 +27,67 @@ const styles = {
   };
 const data = {
     "name": "Saurabh Ghuge",
-    "roles": ["an Software Engineer", "a Full-Stack Developer", "a Freelancer"]};
+    "roles": ["a Software Engineer", "a Full-Stack Developer", "a Freelancer"]};
 
 const FrontPage= ()=>{
     return (
         <div className="frontpage bg-dark  " id = 'Home'>
             <div className="homepage">
-            <section className="bg-dark text-center text-light p-5  ">
-                <div className="container">
-                {/* <div className="d-sm-flex"> */}
-                        <div className="text-start">
-                            {/* <h1>Hi, I am a <span className="text-warning">Software Engineer</span></h1> */}
-                            {/* <p className="text-center"> */}
-                            {/* </p> */}
-                            {/* <div className="mb-2">
-                                <a href="https://github.com/saurabhghuge">
-                                    <Button variant="primary" size="md">
-                                        Know More!
-                                    </Button>
-                                </a>
+                <section className="bg-dark text-center text-light p-5  ">
+                    <div className="container">
+                    {/* <div className="d-sm-flex"> */}
+                            <div className="text-start">
+                                {/* <h1>Hi, I am a <span className="text-warning">Software Engineer</span></h1> */}
+                                {/* <p className="text-center"> */}
+                                {/* </p> */}
+                                {/* <div className="mb-2">
+                                    <a href="https://github.com/saurabhghuge">
+                                        <Button variant="primary" size="md">
+                                            Know More!
+                                        </Button>
+                                    </a>
 
-                            </div> */}
+                                </div> */}
 
-                            {/* <Fade> */}
-                                <div style={styles.mainContainer}>
-                                    <h1 style={styles.nameStyle}>{data?.name}</h1>
-                                    <div style={{ flexDirection: 'row' }}>
-                                        <div style={styles.inlineChild}>
-                                            <h2 >I&apos;m&nbsp;</h2></div>
+                                {/* <Fade> */}
+                                    <div style={styles.mainContainer}>
+                                        <h1 style={styles.nameStyle}>{data?.name}</h1>
+                                        <div style={{ flexDirection: 'row' }}>
                                             <div style={styles.inlineChild}>
-                                            <Typewriter
-                                                options={{
-                                                loop: true,
-                                                autoStart: true,
-                                                strings: data?.roles,
-                                                }}
-                                            />
+                                                <h2 >I&apos;m&nbsp;</h2>
+                                            </div>
+                                            <div style={styles.inlineChild}>
+                                                <Typewriter
+                                                    options={{
+                                                    loop: true,
+                                                    autoStart: true,
+                                                    strings: data?.roles,
+                                                    }}
+                                                />
                                             </div>
                                         </div>
-                 
-                                    <div className="scrollDownBox" >
+                    
+                                        <div className="scrollDownBox" >
 
-                                        <a href="#About">
-                                            <Button variant="primary" size="md">
-                                            Scroll Down 
-                                            </Button>
-                                        </a>
+                                            <a href="#About">
+
+                                                <Button variant="primary" size="md">
+
+                                                    Scroll Down &nbsp;
+
+                                                    <AiOutlineArrowDown style={{ marginBottom: "2px" }} />
+
+                                                </Button>
+                                            </a>
+                                        </div>
+                                        {/* <Social /> */}
                                     </div>
-                                    {/* <Social /> */}
-                                </div>
-                                {/* </Fade> */}
+                                    {/* </Fade> */}
+                            </div>
+                            {/* <CoderImg className="img-fluid w-50" /> */}
                         </div>
-                        {/* <img src={CoderImg} alt="horse" className="img-fluid w-50" /> */}
-
-                        {/* <CoderImg className="img-fluid w-50" /> */}
-                    </div>
-                {/* </div> */}
-            </section>
-            {/* <Projects />
-            <Experience/>
-            <Blogs/> */}
+                    {/* </div> */}
+                </section>
             </div>
         </div>
         
